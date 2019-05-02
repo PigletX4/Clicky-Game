9bf612ba41ac0ddd478b493960dc4cb4da416bda
Auri <aurihrp@hotmail.com>
2019-05-02 04:06:22 -0700

    Updates

---
+++ precache-manifest.0201d13d6d9404b74795af24af50c6db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f46434caa0dca1847904b0fa044c92c5",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "2eb50f2063d489ddd68f",
+    "url": "/Clicky-Game/static/css/main.e6d80394.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/Clicky-Game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "2eb50f2063d489ddd68f",
+    "url": "/Clicky-Game/static/js/main.e6f09d38.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "c1f81144897fc5acffc67de8febd9690",
+    "url": "/Clicky-Game/static/media/Saiyan-Sans.c1f81144.ttf"
+  }
+]);
